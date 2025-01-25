dd3fabd4fb868fa5719fcec787751501caab7bb7
David Eliason <davethemaker@gmail.com>
2025-01-25 14:17:59 -0800

    add ec2 resource within us-west-2a

---
+++ main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# configure the aws provider
+provider "aws" {
+  shared_config_files      = ["~/.aws/config"]
+  shared_credentials_files = ["~/.aws/credentials"]
+  profile                  = "vscodeaws"
+  region                   = "us-west-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
