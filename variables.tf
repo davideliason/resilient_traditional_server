0a4875f4208faadf8270b720879067dca9efda9e
David Eliason <davethemaker@gmail.com>
2025-01-26 07:56:41 -0800

    create variable to decouple hard-coded variable values for config infrastructure resources

--- variables.tf
+++ variables.tf
@@ -1,6 +1,27 @@
-variable "instance_name" {
-  description = "The name of the EC2 instance"
+variable "cidr_block" {
+  description = "The CIDR block for the VPC"
   type        = string
-  default     = "ExampleAppServerInstance"
+  default     = "10.16.0.0/16"
 
+}
+
+variable "resource_tags" {
+  description = "Tags to set for all resources"
+  type        = map(string)
+  default = {
+    project     = "my-ec2",
+    environment = "dev"
+  }
+}
+
+variable "pub_subnet_cidr_blocks" {
+  description = "CIDR blocks for public subnets"
+  type        = list(string)
+  default     = ["10.16.0.0/20", "10.16.16.0/20"]
+}
+
+variable "availability_zones" {
+  description = "Availability zones for public subnets"
+  type        = list(string)
+  default     = ["us-west-2a", "us-west-2b"]
 }
